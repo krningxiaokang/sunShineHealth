1c0da051e402844d6de364a99ff61e9120359d30
kr <admin@krdeMac-mini.local>
2016-12-02 16:21:20 +0800

    Initial Commit

---
+++ test 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test 123
+//
+//  Created by kr on 2016/12/2.
+//  Copyright © 2016年 凯如科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
